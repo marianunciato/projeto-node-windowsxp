df0a0aba11c16833a1e1dc3e279bf67bb63f91b8
Maria Eduarda <anunciatomariaeduarda8@gmail.com>
2024-08-30 11:19:48 -0300

    first commit: full project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div className="App w-screen h-screen flex justify-center items-center flex-wrap">
       <div>
         <div className="bg-sky-600 w-full p-2 rounded-t-xl flex justify-between">
-          <h1 className="titulo-chat font-medium text-white text-lg ml-2">Chat</h1>
+          <h1 className="titulo-chat font-medium text-white text-lg ml-2">Chat room</h1>
           <span class="material-symbols-outlined flex justify-center items-center rounded-md bg-red-700 hover:bg-red-800 cursor-pointer border-2 text-white border-white mr-[1px]" onClick={handleRefresh}>
               close
           </span>
